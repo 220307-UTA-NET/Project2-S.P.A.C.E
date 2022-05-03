e6d924948d7ec304b924a10ba1b4d8e67f9775bb
tnguyen1821 <tuanknguyen98@gmail.com>
2022-05-02 23:51:24 -0700

    Added class and IDE into the headers

--- Space/js/script.js
+++ Space/js/script.js
@@ -3,6 +3,16 @@ function DisplayCSOverview()
     $('#content_insert').load('/pages/csoverview.html');
 }
 
+function DisplayIDE()
+{
+    $('#content_insert').load('/pages/IDE.html');
+}
+
+function DisplayClass()
+{
+    $('#content_insert').load('/pages/Class.html')
+}
+
 function DisplayDTP()
 {
     $('#content_insert').load('/pages/DTP.html');
