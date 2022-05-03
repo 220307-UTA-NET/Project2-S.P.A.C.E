db4ce92a6e73b15321f48b2e711cca06c33e5514
tnguyen1821 <tuanknguyen98@gmail.com>
2022-05-03 15:21:00 -0700

    Added comments and Console header

--- Space/js/script.js
+++ Space/js/script.js
@@ -13,6 +13,16 @@ function DisplayClass()
     $('#content_insert').load('/pages/Class.html')
 }
 
+function DisplayComments()
+{
+    $('#content_insert').load('/pages/Comments.html')
+}
+
+function DisplayConsole()
+{
+    $('#content_insert').load('/pages/Console.html')
+}
+
 function DisplayDTP()
 {
     $('#content_insert').load('/pages/DTP.html');
